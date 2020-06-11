d9e2a5ceeb45211c87f7c2359413a3511ed09e01
Lina Maria <linamaria126@gmail.com>
2020-06-11 01:36:37 +0000

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main -mi primera linea de código
+* Return: retorna a cero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
