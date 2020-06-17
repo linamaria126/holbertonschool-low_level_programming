8a5fa1b04417fe5bf3a0ffe17f4a7bd97c44f5a8
Lina Maria <linamaria126@gmail.com>
2020-06-17 02:20:15 +0000

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_alphabet - print alphabet function
+*/
+
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		++x;
+	}
+	_putchar('\n');
+
+}
