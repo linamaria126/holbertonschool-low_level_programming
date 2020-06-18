5914dc1a0d724a632f3b8081098c254367229397
Lina Maria <linamaria126@gmail.com>
2020-06-17 21:40:56 -0500

    0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - main function
+ * Return: Return 0
+ */
+int positive_or_negative(int n)
+{
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+
+	if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+
+	if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	return (0);
+}
+
