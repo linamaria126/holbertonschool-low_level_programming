edd79981528e4464322a4705c24477ce0f27abd9
Lina Maria <linamaria126@gmail.com>
2020-06-17 21:22:43 -0500

    fix 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,46 @@
 #include "holberton.h"
 
+/**
+* table - print table
+* @n: integer
+*/
+void table(int t)
+{
+	int m,r;
+	for (m = 0; m <= 9; m++)
+	{
+		r = t * m;
+		if (m != 0)
+		{
+			if (r > 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+
+		if (r > 9)
+		{
+			_putchar ('0' + r / 10);
+			_putchar ('0' + r % 10);
+		}
+		else
+		{
+			_putchar ('0' + r);
+		}
+
+		if (m != 9)
+		{
+			_putchar(',');
+		}
+
+	}
+
+}
 
 /**
 * times_table - print tables to 0 at 9
@@ -7,49 +48,12 @@
 
 void times_table(void)
 {
-	int t, m, r;
+	int t;
 
 	for (t = 0; t <= 9; t++)
 	{
-
-		for (m = 0; m <= 9; m++)
-		{
-			r = t*m;
-
-			if(m != 0){
-
-				if(r > 9)
-				{
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-
-			}
-
-			if(r > 9){
-
-				_putchar ('0' + r / 10);
-				_putchar ('0' + r % 10);
-
-			}
-			else
-			{
-				_putchar ('0' + r);
-			}
-
-			if(m != 9){
-				_putchar(',');
-			}
-
-
-		}
-
+		table(t);
 		_putchar('\n');
-
 	}
 
 
